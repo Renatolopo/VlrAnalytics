d2a76bd81e25fea9a43b96511ff4d5a3bd30777f
Renatolopo <renatolopo62@gmail.com>
2021-12-30 07:38:52 -0300

    coletando a tabela com o pandas

--- src/scraping.py
+++ src/scraping.py
@@ -1,31 +1,7 @@
-# %%
-import requests
-from bs4 import BeautifulSoup
+import pandas as pd
+import lxml
 
-# %%
-r = requests.get('https://www.vlr.gg/stats')
-soup = BeautifulSoup(r.content, 'html.parser')
-table = soup.find_all('tr')
-table
-# %%
-def get_info(player):
-    """Retorna as informações dos players"""
-    # nome dos players
-    name = player.find('div', attrs={'class':'text-of'}).text
-    # agentes que os players jogam
-    agents = player.find('td', attrs={'class':'mod-agents'}).div.find_all('img')
-    # quantidade de rounds jogados
-    rdn = player.find('td', attrs={'class':'mod-rnd'}).text
-    # pontuação media de combate
-    acs = player.find('td', attrs={'class':'mod-color-sq mod-acs'}).text
-    
-
-
-# %%
-for player in table:
-    try:
-        imgs = player.find('td', attrs={'class':'mod-color-sq mod-acs'}).text
-        print(imgs)
-        print('**')
-    except:
-        print('null')
+# coletando a tabela com os dados no pandas e exportando em csv
+df = pd.read_html('https://www.vlr.gg/stats')
+df = df[0]
+df.to_csv("../data/stats_vlr.csv")
